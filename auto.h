b409290d226c44e2273582cc3d95a4b9a094c369
3andan4 <maxime.le-besnerais@epitech.eu>
2022-12-01 10:30:56 +0100

    :sparkles: Temp auto.h: 	    assets(header file template): lots of libraries useful for TK1 projects :camera:

---
+++ auto.h
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2022
+** my_ls
+** File description:
+** lib.h
+*/
+
+#ifndef AUTO_H_
+    #define AUTO_H_
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <stdarg.h>
+    #include <string.h>
+    #include <time.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <dirent.h>
+    #include <pwd.h>
+    #include <grp.h>
+    #include <stdbool.h>
+    #include <fcntl.h>
+    #include <SFML/Graphics.h>
+    #include <SFML/Audio.h>
+    #include <SFML/Window.h>
+    #include <SFML/System.h>
+    #include <SFML/Config.h>
+    #include <SFML/Network.h>
+    #include <ncurses.h>
+    %%LIB
+#endif
