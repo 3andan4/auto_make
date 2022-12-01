daf3ad31bd76f536d1f924e92a1049e42be9c04a
3andan4 <maxime.le-besnerais@epitech.eu>
2022-12-01 01:29:33 +0100

    :sparkles: Other templates header.h lib.h: 	    assets(Files): Used as template :camera:

---
+++ header.h
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT
+** Auto HEADER
+*/
+
+#ifndef %HEADER_H_
+    #define %HEADER_H_
+    #include "lib.h"
+    %%FUNCTIONS
+#endif
---
+++ lib.h
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** my_ls
+** File description:
+** lib.h
+*/
+
+#ifndef MY_LS_LIB_H
+    #define MY_LS_LIB_H
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <stdarg.h>
+    #include <string.h>
+    #include <time.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <dirent.h>
+    #include <pwd.h>
+    #include <grp.h>
+    #include <stdbool.h>
+    #include <fcntl.h>
+#endif
